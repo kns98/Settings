678b43586469c0c2cc5a35a7af6b330e8e6cfb99
kns98 <kns98@yahoo.com>
2022-08-24 22:00:19 +0800

    Add project files.

---
+++ SampleApplicationSettingsDialog/MainForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using SampleApplicationSettingsDialog.Properties;
+
+namespace SampleApplicationSettingsDialog
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // データグリッドを更新
+            dataGridView1.Columns.Add("Name", "Name");
+            dataGridView1.Columns.Add("Value", "Value");
+            dataGridView1.Columns[1].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Settings.Default.SettingChanging += (s, e) => updateDataGrid();
+            updateDataGrid();
+        }
+
+        /// <summary>
+        ///     プロパティの編集画面を開く
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            using (var form = new SettingsEditForm())
+            {
+                form.ShowDialog();
+            }
+        }
+
+        /// <summary>
+        ///     DataGridを最新の値に更新する
+        /// </summary>
+        private void updateDataGrid()
+        {
+            dataGridView1.Rows.Clear();
+            foreach (SettingsProperty property in Settings.Default.Properties)
+                if (Settings.Default[property.Name] is StringCollection)
+                {
+                    var collection = (Settings.Default[property.Name] as StringCollection).Cast<string>();
+                    dataGridView1.Rows.Add(property.Name, string.Join("\n", collection));
+                }
+                else
+                {
+                    dataGridView1.Rows.Add(property.Name, Settings.Default[property.Name].ToString());
+                }
+        }
+
+        /// <summary>
+        ///     user.configのディレクトリを開く。
+        ///     存在しない場合があるので、存在するところまでパスを削って開く
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonOpenDir_Click(object sender, EventArgs e)
+        {
+            var userConfigPath = ConfigurationManager
+                .OpenExeConfiguration(ConfigurationUserLevel.PerUserRoamingAndLocal).FilePath;
+            var directory = Path.GetDirectoryName(userConfigPath);
+            while (!string.IsNullOrEmpty(directory))
+            {
+                if (Directory.Exists(directory))
+                {
+                    Process.Start(directory);
+                    return;
+                }
+
+                directory = Path.GetDirectoryName(directory);
+            }
+
+            ;
+        }
+    }
+}
---
+++ SampleApplicationSettingsDialog/Settings.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace SampleApplicationSettingsDialog.Properties
+{
+    internal sealed partial class Settings
+    {
+        public Settings()
+        {
+            // PropertyGridのCategoryを設定する
+            var categoryTable = new Dictionary<string, Attribute>
+            {
+                { nameof(BoolSetting), new CategoryAttribute("組み込みのデータ型") },
+                { nameof(StringSetting), new CategoryAttribute("組み込みのデータ型") },
+                { nameof(StringCollectionSetting), new CategoryAttribute("複合データ型") },
+                { nameof(DateTimeSetting), new CategoryAttribute("複合データ型") },
+                { nameof(IntSetting), new CategoryAttribute("組み込みのデータ型") }
+            };
+            addAttribute(categoryTable);
+
+            // PropertyGridのHelpテキストを設定する
+            var descriptionTable = new Dictionary<string, Attribute>
+            {
+                { nameof(BoolSetting), new DescriptionAttribute("bool型の設定") },
+                { nameof(StringSetting), new DescriptionAttribute("string型の設定") },
+                { nameof(StringCollectionSetting), new DescriptionAttribute("複数のstring型の設定") },
+                { nameof(DateTimeSetting), new DescriptionAttribute("DateTime型の設定") },
+                { nameof(IntSetting), new DescriptionAttribute("int型の設定") }
+            };
+            addAttribute(descriptionTable);
+        }
+
+        /// <summary>
+        ///     プロパティに属性を追加する
+        /// </summary>
+        /// <param name="attributeTable">Key:プロパティ名、Value:追加する属性</param>
+        private void addAttribute(Dictionary<string, Attribute> attributeTable)
+        {
+            if (attributeTable == null) return;
+
+            var properties = TypeDescriptor.GetProperties(this);
+            foreach (PropertyDescriptor p in properties)
+            {
+                Attribute attribute;
+                if (attributeTable.TryGetValue(p.Name, out attribute))
+                {
+                    // 属性を追加する。
+                    // 本当はMemberDescriptor.Attributes.Addのようにしたいのだが、Attributes属性はgetだけ定義されている。
+                    // そのためリフレクションを使って属性を追加する
+                    var fi = p.Attributes.GetType()
+                        .GetField("_attributes", BindingFlags.NonPublic | BindingFlags.Instance);
+                    var attrs = fi.GetValue(p.Attributes) as Attribute[];
+                    var listAttr = new List<Attribute>();
+                    if (attrs != null) listAttr.AddRange(attrs);
+                    listAttr.Add(attribute);
+                    fi.SetValue(p.Attributes, listAttr.ToArray());
+                }
+            }
+        }
+    }
+}
