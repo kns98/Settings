b2597110a4f8df45701423bd8bb0a9d8a4111343
kns98 <kns98@yahoo.com>
2022-08-25 22:45:09 +0800

    tr

--- SampleApplicationSettingsDialog/MainForm.Designer.cs
+++ SampleApplicationSettingsDialog/MainForm.Designer.cs
@@ -39,7 +39,7 @@
             // 
             this.buttonOpenEdit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonOpenEdit.Location = new System.Drawing.Point(412, 360);
-            this.buttonOpenEdit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonOpenEdit.Margin = new System.Windows.Forms.Padding(4);
             this.buttonOpenEdit.Name = "buttonOpenEdit";
             this.buttonOpenEdit.Size = new System.Drawing.Size(165, 31);
             this.buttonOpenEdit.TabIndex = 0;
@@ -53,9 +53,9 @@
             this.label1.Location = new System.Drawing.Point(16, 9);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 16);
+            this.label1.Size = new System.Drawing.Size(100, 16);
             this.label1.TabIndex = 2;
-            this.label1.Text = "現在の設定";
+            this.label1.Text = "Current Settings";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // dataGridView1
@@ -67,7 +67,7 @@
             this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(19, 32);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
@@ -81,11 +81,11 @@
             // 
             this.buttonOpenDir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonOpenDir.Location = new System.Drawing.Point(19, 360);
-            this.buttonOpenDir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonOpenDir.Margin = new System.Windows.Forms.Padding(4);
             this.buttonOpenDir.Name = "buttonOpenDir";
             this.buttonOpenDir.Size = new System.Drawing.Size(224, 31);
             this.buttonOpenDir.TabIndex = 0;
-            this.buttonOpenDir.Text = "Show in Explorer";
+            this.buttonOpenDir.Text = "Open in Explorer";
             this.buttonOpenDir.UseVisualStyleBackColor = true;
             this.buttonOpenDir.Click += new System.EventHandler(this.buttonOpenDir_Click);
             // 
@@ -98,7 +98,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonOpenDir);
             this.Controls.Add(this.buttonOpenEdit);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
